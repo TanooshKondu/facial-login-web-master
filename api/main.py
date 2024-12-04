4d79efaa23c0d3fe0d20c55c4606e19db791b8ab
Tanoosh Kondu <kondutanoosh04@gmail.com>
2024-12-04 22:47:52 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -84,5 +84,5 @@ def result():
     result = result[:len(result)-4]
     return render_template('result.html', result=result)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
